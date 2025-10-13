256424298d9c92ddab02b7940868cf06ebe8c771
tomasmaraval <maravaltomas06@gmail.com>
2025-10-13 20:41:18 -0300

    .

--- dom.js
+++ dom.js
@@ -3,39 +3,34 @@ function registrarse (){
     window.location.assign('registro.html')
 }
 
-let password = document.getElementById('contraseña');
+let contraseña = document.forms['registro']['contraseña']
+let confirmar = document.forms['registro']['confirmar'];
 let confirmarDiv = document.getElementById('confirm-container');
+let eyeBtn = document.getElementById("eyeBtn");
 
-password.addEventListener('input', function() {
-    if (password.value.length > 0) {
+contraseña.addEventListener('input', function() {
+    if (contraseña.value.length > 0) {
         confirmarDiv.style.display = 'block';
     } else {
         confirmarDiv.style.display = 'none';
     }
 });
 
-let eyeBtn = document.getElementById("eyeBtn");
-let input = document.getElementById("confirmar");
 
 eyeBtn.addEventListener("click", () => {
-  if (input.type === "password" && password.type === 'password') {
-    input.type = "text";
-    password.type = "text";
-    eyeBtn.textContent = "Ocultar contraseñas "; 
+  if (confirmar.type === "password" && contraseña.type === 'password') {
+    confirmar.type = "text";
+    contraseña.type = "text";
+    eyeBtn.textContent = "Ocultar contraseñas"; 
   } else {
-    input.type = "password";
-    password.type = "password"
-    eyeBtn.textContent = "Visualizar contraseñas ";
+    confirmar.type = "password";
+    contraseña.type = "password"
+    eyeBtn.textContent = "Visualizar contraseñas";
   }
 });
 
-function registro (){
-    let user = document.getElementById('usuario').value
-    let nombre = document.getElementById('nombre').value
-    let apellido = document.getElementById('apellido').value
-    let code = document.getElementById('codigoFamiliar').value
-    let confirmación = document.getElementById('confirmar');
-
+function validarRegistro (event){
+    if (event) event.preventDefault();
     let num = false
     let esp = false
     let carN = false
@@ -43,22 +38,15 @@ function registro (){
     let nums = '0123456789'
     let esps = '/?.><,;:][}{=_-+*&^%$#@!~'
     let numEsps = '0123456789/?.><,;:][}{=_-+*&^%$#@!~'
+    document.getElementById('errorUser').innerHTML = '';
+    document.getElementById('errorNom').innerHTML = '';
+    document.getElementById('errorApe').innerHTML = '';
+    document.getElementById('errorCode').innerHTML = '';
+    document.getElementById('errorPass').innerHTML = '';
+    document.getElementById('errorConf').innerHTML = '';
 
-    for (let i = 0; i < password.value.length; i++){
-        for (let j = 0; j < nums.length; j++){
-            if (password.value[i] == nums[j]){
-                num = true
-                console.log(num)
-            } 
-        }
-        for (let k = 0; k < esps.length; k++){
-            if (password.value[i] == esps[k]){
-                esp = true
-                console.log(esp)
-            } 
-        }
-    }
-
+    let user = document.forms['registro']['usuario'].value
+    let nombre = document.forms['registro']['nombre'].value
     for (let i = 0; i < nombre.length; i++){
         for (let k = 0; k < numEsps.length; k++){
             if (nombre[i] == numEsps[k]){
@@ -67,6 +55,7 @@ function registro (){
         }
     }
 
+    let apellido = document.forms['registro']['apellido'].value
     for (let i = 0; i < apellido.length; i++){
         for (let k = 0; k < numEsps.length; k++){
             if (apellido[i] == numEsps[k]){
@@ -75,78 +64,123 @@ function registro (){
         }
     }
 
-    if (code.length != 6){
-        alert('Ingrese codigo de 6 digitos')
-    } else if (carA == true || carN == true){
-        alert('Ingrese solo letras en Nombre, Apellido')
-    } else if (num == false || esp == false || password.value.length <= 6){
-        alert('Incluya al menos un número y un caracter especial. Minimo 6 caracteres.')
-    } else if (num == true && esp == true && password.value == confirmación.value && code.length == 6){
-        if(document.getElementById("usuario_select").value == "hijo"){
-            alert(`Bienvenido, ${user}`)
-            window.location.assign('inicio.html')
-            localStorage.setItem('user', user)
-            localStorage.setItem('code', code)
-            localStorage.setItem('padre', false)
-        }
-        else if (document.getElementById("usuario_select").value == "padre"){
-            alert(`Bienvenido, ${user}`)
-            window.location.assign('iniciopadre.html')
-            localStorage.setItem('user', user)
-            localStorage.setItem('code', code)
-            localStorage.setItem('padre', true)
+    let code = document.getElementById('codigoFamiliar').value
 
+    for (let i = 0; i < contraseña.value.length; i++){
+        for (let j = 0; j < nums.length; j++){
+            if (contraseña.value[i] == nums[j]){
+                num = true
+            } 
         }
-    } else {
-        alert('Contraseñas no coincidentes.')
+        for (let k = 0; k < esps.length; k++){
+            if (contraseña.value[i] == esps[k]){
+                esp = true
+            } 
+        }
     }
-    
-    
+
+    let valido = true
+
+    if (user == 'existe'){
+        document.getElementById('errorUser').innerHTML = 'Usuario existente'
+        valido = false
+    }
+    if (carN == true){
+        document.getElementById('errorNom').innerHTML = 'Ingrese solo letras'
+        valido = false
+    }
+    if (carA == true){
+        document.getElementById('errorApe').innerHTML = 'Ingrese solo letras'
+        valido = false
+    }
+    if (code.length != 6){
+        document.getElementById('errorCode').innerHTML = 'Codigo de 6 digitos'
+        valido = false
+    }
+    if (num == false || esp == false || contraseña.value.length < 6){
+        document.getElementById('errorPass').innerHTML = 'Incluya al menos un número y un caracter especial. Minimo 6 caracteres.'
+        valido = false
+    }
+    if (contraseña.value != confirmar.value) {
+        document.getElementById('errorConf').innerHTML = 'Contraseñas no coincidentes.'
+        valido = false
+    }
+    if (valido != true){
+        return false
+    }
+
+    if(document.forms['registro']['select'].value == "hijo"){
+        alert(`Bienvenido, ${user}`)
+        window.location.assign('inicio.html')
+        localStorage.setItem('user', user)            
+        localStorage.setItem('code', code)
+        localStorage.setItem('padre', false)
+        return true
+    }
+    else if (document.forms['registro']['select'].value == "padre"){
+        alert(`Bienvenido, ${user}`)            
+        window.location.assign('iniciopadre.html')
+        localStorage.setItem('user', user)
+        localStorage.setItem('code', code)            
+        localStorage.setItem('padre', true)
+        return true
+
+    }
 }
 
-function loguearse (){
+
+function loguearse (event){
+    if (event) event.preventDefault();
     let u = "minombre"
     let p = "contraseña123"
 
-    let user = document.getElementById('usuario')
-    let password = document.getElementById('contraseña')
+    let user = document.forms['login']['usuario'].value
+    let password = document.forms['login']['contraseña'].value
 
+    let valido = true
+
+    
+    if (user.value.length < 1 || password.value.length < 1){
+        alert(`Completar campos`)
+        valido = false
+        if (user.value.length < 1){
+            user.style.border =  "2px solid #ff0000ff";            
+        }
+        if (password.value.length < 1){
+            password.style.border =  "2px solid #ff0000ff";            
+        }
+    }
+
+    if (user.value != u){
+        document.getElementById('errorUser').innerHTML = 'Usuario no existente'
+        valido = false
+        user.value = ""
+        password.value = ""
+        user.style.border =  "2px solid #ff0000ff";
+        password.style.border =  "2px solid #000852";
+    }    
+
+    if (user.value == u && password.value != p){
+        document.getElementById('errorPass').innerHTML = 'Contraseña incorrecta.'
+        valido = false
+        password.value = ""
+        user.style.border =  "2px solid #000852";
+        password.style.border =  "2px solid #ff0000ff";     
+    }
+
+    if (valido != true){
+        return false
+    }
 
     if (user.value == u && password.value == p){
         alert(`Bienvenido, ${user.value}`)
         window.location.assign('inicio.html')
         localStorage.setItem('user', user.value)
-        }
-
-    else if (user.value.length < 1 || password.value.length < 1){
-            alert(`Completar campos`)
-            if (user.value.length < 1){
-                user.style.border =  "2px solid #ff0000ff";
-            }
-            if (password.value.length < 1){
-                password.style.border =  "2px solid #ff0000ff";
-            }
-        }
-
-    else if (user.value != u){
-            alert(`No se encontro usuario`)
-
-            user.value = ""
-            password.value = ""
-
-            user.style.border =  "2px solid #ff0000ff";
-            password.style.border =  "2px solid #000852";
-        }    
-
-    else if (user.value == u && password.value != p){
-            alert(`Contraseña Incorrecta`)
-
-            password.value = ""
-
-            user.style.border =  "2px solid #000852";
-            password.style.border =  "2px solid #ff0000ff";
-        }
+        return true
+        
     }
+
+}
 
 
 function actividad (){
