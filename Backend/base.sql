60918cf36d29eb7ffda33b0c27390e17d5424fb5
tomasmaraval <maravaltomas06@gmail.com>
2025-11-03 11:58:19 -0300

    .

--- Backend/base.sql
+++ Backend/base.sql
@@ -4,6 +4,10 @@ use tomasmaraval_familybank;
 
 SELECT * FROM usuarios
 
+SELECT * FROM actividades
+
+SELECT * FROM solicitudes
+
 /*
 INSERT INTO `usuarios` (`id`, `nombres`, `apellido`, `codigo_Familiar`, `es_padre`, `saldo`,  `contrasenia`, `nombre_usuario`, `admitido`, `img`) 
 VALUES (Null, 'Tomas', 'Maraval', 123456, True, 100000, 'AguanteCentral1!', 'tomimaraval', True, 'foto.jpg');
@@ -34,4 +38,7 @@ SELECT * FROM `usuarios` WHERE `codigo_Familiar`= 123456
 
 SELECT * FROM `usuarios` WHERE `codigo_Familiar`= 123654
 
-INSERT into 'actividades' 
+INSERT into `actividades` (`id`, `emisor`, `receptor`, `motivo`, `fecha`, `monto`) VALUES (Null, 2, 6, 'Pago uber', '2025-11-03T10:43', 25000), 
+(Null, 1, 3, 'Compra Starbucks Iced Vanilla Latte Macchiatto with whipped cream honey mustard', '2025-11-01T00:49', 2314567865432), 
+(Null, 3, 10, 'Entradas', '2025-11-03T10:43', 10000), 
+(Null, 2, 11, 'Vario', '2025-11-03T10:43', 5000)
