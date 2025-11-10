fc4625201e46fb33cf42259640089a99197822e2
feliorlando <felicitasorlando@uca.edu.ar>
2025-11-09 22:11:10 -0300

    .

--- Backend/base.sql
+++ Backend/base.sql
@@ -1,4 +1,4 @@
--- Active: 1761570068921@@mysql-tomasmaraval.alwaysdata.net@3306@tomasmaraval_familybank
+-- Active: 1761576574446@@mysql-tomasmaraval.alwaysdata.net@3306@tomasmaraval_familybank
 SHOW DATABASES;
 use tomasmaraval_familybank;
 
@@ -10,7 +10,7 @@ SELECT * FROM actividades
 
 SELECT * FROM solicitudes
 
-/*
+
 INSERT INTO `usuarios` (`id`, `nombres`, `apellido`, `codigo_Familiar`, `es_padre`, `saldo`,  `contrasenia`, `nombre_usuario`, `admitido`, `img`) 
 VALUES (Null, 'Tomas', 'Maraval', 123456, True, 100000, 'AguanteCentral1!', 'tomimaraval', True, 'foto.jpg');
 
@@ -19,9 +19,9 @@ VALUES (Null, 'Felicitas', 'Orlando Linares', 654321, True, 500000, 'Linares123!
 
 INSERT INTO `usuarios` (`id`, `nombres`, `apellido`, `codigo_Familiar`, `es_padre`, `saldo`,  `contrasenia`, `nombre_usuario`, `admitido`, `img`) 
 VALUES (Null, 'Maria Valentina', 'Morello', 123654, True, 1000000, 'Vmorello234!', 'valentina.morello', True, 'fotodeperfil.jpg');
-*/
 
-/* AGREGADO DE HIJOS 
+
+/* AGREGADO DE HIJOS */
 INSERT INTO `usuarios` (`id`, `nombres`, `apellido`, `codigo_Familiar`, `es_padre`, `saldo`,  `contrasenia`, `nombre_usuario`, `admitido`, `img`) 
 VALUES (Null, 'Sol', 'Morello', 123654, False, 10, 'Sol1!', 'solmorello', True, '"/Images/fotos_de_perfil/fotosol.jpg"'),
 (Null, 'Juana', 'Morello', 123654, False, 99999, 'Juanita01!', 'juanimorello', True, '"/Images/fotos_de_perfil/fotojuani.jpg"');
@@ -33,7 +33,7 @@ VALUES (Null, 'Juan', 'Maraval', 123456, False, 100, 'JuanMar12', 'juanmaraval',
 INSERT INTO `usuarios` (`id`, `nombres`, `apellido`, `codigo_Familiar`, `es_padre`, `saldo`,  `contrasenia`, `nombre_usuario`, `admitido`, `img`) 
 VALUES (Null, 'Nicolas', 'Orlando Linares', 654321, False, 100, 'Nico0090605', 'nico.ol', True, '"/Images/fotos_de_perfil/fotonico.jpg"'), 
 (Null, 'Violeta', 'Orlando Linares', 654321, False, 1000, 'Viole23', 'violeorlando', True, '"/Images/fotos_de_perfil/fotovilu.jpg"');
-*/
+
 
 /* MOSTRAR POR FAMILIA*/
 
@@ -57,7 +57,6 @@ INSERT into `actividades` (`id`, `emisor`, `receptor`, `motivo`, `fecha`, `monto
 
 
 /* actividades por emisor*/
-
 SELECT emisor.nombre_usuario AS Emisor,  receptor.nombre_usuario AS Receptor,
 actividades.monto AS Monto, actividades.motivo as Motivo, actividades.fecha as Fecha FROM actividades
 INNER JOIN usuarios AS emisor ON actividades.emisor = emisor.id
@@ -93,7 +92,6 @@ INNER JOIN usuarios ON solicitudes.id_usuario = usuarios.id
 WHERE solicitudes.estado = 'pendiente';
 
 
-
 /*Buscar usuario y contrasenia PARA LOGIN*/
 select nombre_usuario, contrasenia from usuarios where nombre_usuario = 'solmorello' and contrasenia = 'Sol1!'
 
@@ -105,7 +103,7 @@ values (null, 'Juan Ramon', 'Ramirez', 999999, 1, 0, 'juanRa02.', 'juanRamirez',
 update usuarios set saldo = 100000 where id = 16 
 
 /*Modificar datos del usuario*/
-update usuarios set nombres = 'Juana Ramona', apellido = 'Di Maria' where id = 16 
+update usuarios set nombres = 'Juana Ramona', apellido = 'Di Maria' where id = 16 ;
 
 /*Crear actividades*/
 insert into actividades (`id`, `emisor`, `receptor`, `motivo`, `fecha`, `monto`) 
@@ -137,4 +135,15 @@ values (null, 11, 20, now(), 'pendiente')
 /*Ver solicitudes de grupo familiar*/
 select monto, estado, fecha, id_usuario.codigo_familiar as familia, id_usuario.nombres as hijo from solicitudes
 inner join usuarios as id_usuario on solicitudes.id_usuario = id_usuario.id
-where id_usuario.codigo_familiar = 123654 order by fecha asc
+where id_usuario.codigo_familiar = 123654 order by fecha asc ;
+
+
+/*Ver hijos aun por admitir (por ID)*/
+SELECT padre.nombre_usuario as PADRE, hijos.nombre_usuario as username, hijos.nombres as Nombre, hijos.apellido as Apellido 
+FROM usuarios padre, usuarios hijos
+WHERE padre.es_padre = 1 AND hijos.es_padre = 0 AND padre.codigo_Familiar = hijos.codigo_Familiar 
+    AND hijos.admitido = 0 AND padre.id=3;
+
+/*Aceptar hijos*/
+UPDATE usuarios SET admitido = 1 WHERE usuarios.id = 10 AND usuarios.es_padre = 0;
+
