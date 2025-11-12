f268fde491201b44dbee4e14184cc5eee55d568c
feliorlando <felicitasorlando@uca.edu.ar>
2025-11-12 16:13:53 -0300

    .

--- Backend/base.sql
+++ Backend/base.sql
@@ -1,4 +1,5 @@
 -- Active: 1761576574446@@mysql-tomasmaraval.alwaysdata.net@3306@tomasmaraval_familybank
+
 SHOW DATABASES;
 use tomasmaraval_familybank;
 
--- Backend/python/consultas.py
+++ Backend/python/consultas.py
@@ -8,37 +8,60 @@ def existe_usuario (nombre_usuario):
     mydb = conectarDB(BASE)
     res = consultarDB(mydb,sQuery,val)
     cerrarDB(mydb)
+
+    if res == []:
+        res = False
+    else:
+        res = True
+    return res
+
+def existe_codfam (codfam):
+    sQuery="""
+        SELECT codigo_Familiar FROM usuarios WHERE codigo_Familiar = %s """
+    val = (codfam)
+    mydb = conectarDB(BASE)
+    res = consultarDB(mydb,sQuery,val)
+    cerrarDB(mydb)
+
+    if res == []:
+        res = False
+    else:
+        res = True
     return res
 
 #Usuario nuevo
-def registro(nombre, apellido, codfam, es_padre, saldo=0, contrasenia, nombre_usuario, admitido=0, img):
-    if existe_usuario(nombre_usuario) = 
-    if es_padre == 1:
-        admitido = 1
-        codfam = random.randint(100000, 999999)
+def registro(nombre, apellido, codfam, es_padre, contrasenia, nombre_usuario, img, admitido=0, saldo=0):
+    if not existe_usuario(nombre_usuario):
+        if es_padre == 1:
+            admitido = 1
 
-    sQuery="""
-        INSERT INTO usuario
-        (id, nombres, apellido, codigo_Familiar, es_padre, saldo, contrasenia, nombre_usuario, admitido, img)
-        VALUES
-        (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)"""
-    val = ('Null', nombre, apellido, codfam, es_padre, saldo, contrasenia, nombre_usuario, admitido, img)
-    mydb = conectarDB(BASE)
-    res = ejecutarDB(mydb, sQuery,val)
-    cerrarDB(mydb)
+            codfam = random.randint(100000, 999999)
+            while existe_codfam(codfam):
+                codfam = random.randint(100000, 999999)
+
+            sQuery="""
+            INSERT INTO usuarios
+            (id, nombres, apellido, codigo_Familiar, es_padre, saldo, contrasenia, nombre_usuario, admitido, img)
+            VALUES
+            (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,)"""
+            val = ('Null', nombre, apellido, codfam, es_padre, saldo, contrasenia, nombre_usuario, admitido, img)
+            mydb = conectarDB(BASE)
+            res = ejecutarDB(mydb, sQuery,val)
+            cerrarDB(mydb)
+            
     return res
 
 
 def modificarnombre(nombre_usuario, nuevonombre):
-    sQuery="UPDATE `usuario` SET nombres=%s WHERE nombre_usuario=%s"
+    sQuery="UPDATE usuarios SET nombres=%s WHERE nombre_usuario=%s"
     val = (nuevonombre, nombre_usuario)
     mydb=conectarDB(BASE)
-    res=ejecutarDB(mydb,sQuery,val)       # update
+    res = ejecutarDB(mydb,sQuery,val)       # update
     cerrarDB(mydb)
     return res
 
 def modificarapellido(nombre_usuario, nuevoape):
-    sQuery="UPDATE usuario SET apellido=%s WHERE nombre_usuario=%s"
+    sQuery="UPDATE usuarios SET apellido=%s WHERE nombre_usuario=%s"
     val = (nuevoape, nombre_usuario)
     mydb=conectarDB(BASE)
     res=ejecutarDB(mydb,sQuery,val)       # update
@@ -48,17 +71,17 @@ def modificarapellido(nombre_usuario, nuevoape):
 # ---- PLATA ------
 
 def saldoactual(nombre_usuario):
-    sQuery="SELECT saldo FROM usuario WHERE nombre_usuario=%s"
+    sQuery="SELECT saldo FROM usuarios WHERE nombre_usuario=%s"
     val = (nombre_usuario)
     mydb = conectarDB(BASE)
     res = consultarDB(mydb,sQuery,val)
     cerrarDB(mydb)
     return res
-
+ 
 def transferencia(usuario_emisor, usuario_receptor, monto, motivo, fecha):
     res = None
-    id_emisor = obtener_datos("id","usuario","nombre_usuario",usuario_emisor)
-    id_receptor = obtener_datos("id","usuario","nombre_usuario",usuario_receptor)
+    id_emisor = 
+    id_receptor = 
 
     saldoemisor = saldoactual(usuario_emisor)
     saldoreceptor = saldoactual(usuario_receptor)
