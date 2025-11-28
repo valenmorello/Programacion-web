8910c74f206734d7aab77946bdff017b84916f49
feliorlando <felicitasorlando@uca.edu.ar>
2025-11-27 21:11:40 -0300

    .

--- Backend/python/appConfig.py
+++ Backend/python/appConfig.py
@@ -3,4 +3,4 @@ config={}
 # Directorio del proyecto
 config['project_folder'] = path.dirname(path.realpath(__file__))
 # directorio para subir archivos (con el path completo.
-config['upload_folder'] = path.join(config['project_folder'], 'static/uploads')
+config['upload_folder'] = path.join(config['project_folder'], 'Frontend/static/Images')
